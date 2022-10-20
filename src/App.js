35c05ec7b32e0f2e62baf67d7410c843071d8465
Alex Savi <alex.savi.kv@gmail.com>
2022-10-20 23:50:46 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./auth"
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+    
   );
 }
 
