1caaf926e8aff08ef669806e5c9c9f6bc9d535a5
Alex Savi <alex.savi.kv@gmail.com>
2022-10-28 22:15:15 +0530

    login check

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,14 @@
 import './login.css'
 function Login() {
+    const passw="1234"
+    function getPass()
+    {
+       if(document.querySelector('input').value === passw)
+        {
+            window.alert("correct")
+        }
+        else{ window.alert("Wrong Password")}
+    }
     return (
         <div className='name'>
           <h2 className='font-bold text-[#1d3d6a] justify-center'>Welcome to Towerhill</h2>  
@@ -8,7 +17,7 @@ function Login() {
                 <img src='./images/logo.png' alt="logo"></img><br /><br />
                 <label for="psw">Password</label><br />
                 <input className=' pwd rounded-lg bg-[#61dafb] text-center' type="password" id="psw" name="psw" required></input><br /> 
-                <button className='btn w-40 bg-[#61dafb] shadow-lg shadow-[#cbf1fb] hover:shadow-teal-500'>Submit</button>
+                <button className='btn w-40 bg-[#61dafb] shadow-lg shadow-[#cbf1fb] hover:shadow-teal-500 ' onClick={getPass}>Submit</button>
             </div> 
         </div>
     );
